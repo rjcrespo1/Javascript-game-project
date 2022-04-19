1023ffde46a4f25837955461664474f847a09039
Ryan Crespo <crespoffi@gmail.com>
2022-04-19 18:18:29 -0400

    new project

---
+++ index.js
@@ -0,0 +1,40 @@
+const canvas = document.querySelector('canvas');
+const c = canvas.getContext('2d');
+
+canvas.width = innerWidth;
+canvas.height = innerHeight;
+
+class Player {
+    constructor() {
+        this.position = {
+            x: 100,
+            y: 100
+        };
+        this.velocity = {
+            x: 0,
+            y: 1
+        };
+
+        this.width = 30
+        this.height = 30
+    }
+
+    draw() {
+        c.fillStyle = 'red'
+        c.fillRect(this.position.x, this.position.y, this.width, this.height);
+    };
+
+    update() {
+        this.draw();
+        this.position.y += this.velocity.y
+    };
+}
+
+const player = new Player();
+
+function animate() {
+    requestAnimationFrame(animate)
+    player.update();
+}
+
+animate();
