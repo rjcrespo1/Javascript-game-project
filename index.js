e15c2b4e82655304d8a3af60f04b6bdd1226c1a1
Ryan Crespo <crespoffi@gmail.com>
2022-04-20 16:55:32 -0400

    created player and background movement

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const c = canvas.getContext('2d');
 canvas.width = innerWidth;
 canvas.height = innerHeight;
 
+const gravity = 1.5;
+
 class Player {
     constructor() {
         this.position = {
@@ -12,7 +14,7 @@ class Player {
         };
         this.velocity = {
             x: 0,
-            y: 1
+            y: 0
         };
 
         this.width = 30
@@ -26,15 +28,133 @@ class Player {
 
     update() {
         this.draw();
+        this.position.x += this.velocity.x
         this.position.y += this.velocity.y
+
+        if (this.position.y + this.height + this.velocity.y <= canvas.height) {
+            return this.velocity.y += gravity
+        } else {
+            this.velocity.y = 0
+        }
     };
 }
 
+class Platform {
+    constructor({x, y}) {
+        this.position = {
+            x: x,
+            y: y
+        }
+
+        this.width = 200
+        this.height = 20
+    }
+
+    draw() {
+        c.fillStyle = 'blue'
+        c.fillRect(this.position.x, this.position.y, this.width, this.height);
+    }
+}
+
 const player = new Player();
+const platforms = [new Platform({
+    x: 200, y: 100
+}), new Platform({x: 500, y: 200})];
+// const platform = new Platform();
+
+const keys = {
+    right: {
+        pressed: false
+    },
+    left: {
+        pressed: false
+    }
+}
 
 function animate() {
     requestAnimationFrame(animate)
+    c.clearRect(0, 0, canvas.width, canvas.height)
     player.update();
+    platforms.forEach((platform) => {
+        platform.draw();
+    });
+
+    if (keys.right.pressed && player.position.x < 400) {
+        player.velocity.x = 5;
+    } else if (keys.left.pressed && player.position.x > 100) {
+        player.velocity.x = -5;
+    } else {
+        player.velocity.x = 0
+
+        if (keys.right.pressed) {
+            platforms.forEach((platform) => {
+                platform.position.x -= 5;
+            });
+        } else if (keys.left.pressed) {
+            platforms.forEach((platform) => {
+                platform.position.x += 5;
+            });
+        }
+    };
+
+    // platform collision detection
+    platforms.forEach((platform) => {
+        if (player.position.y + player.height <= platform.position.y && 
+            player.position.y + player.height + player.velocity.y >= platform.position.y && 
+            player.position.x + player.width >= platform.position.x &&
+            player.position.x <= platform.position.x + platform.width) {
+            player.velocity.y = 0
+        }
+    })
 }
 
 animate();
+
+addEventListener('keydown', ({ keyCode }) => {
+    // console.log(keyCode);
+    switch (keyCode) {
+        case 65:
+            console.log('left')
+            keys.left.pressed = true
+            break;
+
+        case 83:
+            console.log('down')
+            break;
+
+        case 68:
+            console.log('right')
+            keys.right.pressed = true
+            break;
+
+        case 32:
+            console.log('up')
+            if (event.repeat) {return}
+            player.velocity.y -= 20;
+            break;
+    }
+});
+
+addEventListener('keyup', ({ keyCode }) => {
+    // console.log(keyCode);
+    switch (keyCode) {
+        case 65:
+            console.log('left')
+            keys.left.pressed = false
+            break;
+
+        case 83:
+            console.log('down')
+            break;
+
+        case 68:
+            console.log('right')
+            keys.right.pressed = false
+            break;
+
+        case 32:
+            console.log('up')
+            player.velocity.y -= 20;
+            break;
+    }
+});
